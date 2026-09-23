6b2b54777eb7d649058c55bc7689d1c4bda4861d
Medas999 <97048764+Medas999@users.noreply.github.com>
2026-09-23 20:55:26 +0300

    Migrate support imports to AndroidX

--- app/src/main/java/com/projection/car/ForgroundService.java
+++ app/src/main/java/com/projection/car/ForgroundService.java
@@ -7,7 +7,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.view.accessibility.AccessibilityEvent;
 
 import static com.projection.car.Utils.log;
