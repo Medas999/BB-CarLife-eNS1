3706fc56485e8218e1b90086bc69c62b16466303
Medas999 <97048764+Medas999@users.noreply.github.com>
2026-09-23 22:49:21 +0300

    Restore Context import for mirror-lite

--- app/src/main/java/com/projection/car/MediaCodecTool.java
+++ app/src/main/java/com/projection/car/MediaCodecTool.java
@@ -1,6 +1,7 @@
 package com.projection.car;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
