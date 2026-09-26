2b068b93350a127c73875d480b332af3c2c397ca
Medas999 <97048764+Medas999@users.noreply.github.com>
2026-09-26 17:17:54 +0300

    fix v3 service imports

--- app/src/main/java/com/projection/car/MediaCodecTool.java
+++ app/src/main/java/com/projection/car/MediaCodecTool.java
@@ -33,6 +33,7 @@ import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import android.media.MediaCodec;
